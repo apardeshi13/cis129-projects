585eb4e20e5d3823f82bcf7a39d7ff9005989a93
apardeshi13 <159093846+apardeshi13@users.noreply.github.com>
2024-02-06 16:20:11 -0700

    Update cis129_lab02_shortStory.py
    Deleted Hello World print statement. Added a short story and comments.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Adi Pardeshi
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print("There was once a Lion who was the enemy of the jungle")
+print("He asked every other animal to sacrifice themselves each day")
+print("On the smart rabbits day, the rabbit takes him to a water well")
+print("And tells him that thheres a second lion that wants to attack you")
+print("The lion attacks his reflection in the water well and drowns")
+print("The rabbit becae a hero for the people")
